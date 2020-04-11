6c3dd85a7c2b69885a6016321582262430cff0f4
Akrit Khanna <34682943+akritkhanna@users.noreply.github.com>
2020-04-12 00:32:25 +0530

    Update server.py

--- server.py
+++ server.py
@@ -3,21 +3,21 @@ import RPi.GPIO as GPIO
 
 app = Flask(__name__)
 
-@app.route('/on/<int:pin>')
-def getOn(pin):
-    GPIO.setmode(GPIO.BCM)
-    GPIO.setup(pin, GPIO.OUT)
-    state = GPIO.input(pin)
-    GPIO.output(pin,GPIO.HIGH)
-    return jsonify({'status':'HIGH', 'pin_no':pin})
-
 @app.route('/off/<int:pin>')
 def getOff(pin):
     GPIO.setmode(GPIO.BCM)
     GPIO.setup(pin, GPIO.OUT)
     state = GPIO.input(pin)
-    GPIO.output(pin,GPIO.LOW)
+    GPIO.output(pin,GPIO.HIGH)
     return jsonify({'status':'LOW', 'pin_no':pin})
+
+@app.route('/on/<int:pin>')
+def getOn(pin):
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setup(pin, GPIO.OUT)
+    state = GPIO.input(pin)
+    GPIO.output(pin,GPIO.LOW)
+    return jsonify({'status':'HIGH', 'pin_no':pin})
 
 
 @app.route('/status/<int:pin>')
@@ -27,12 +27,12 @@ def getStatus(pin):
     state = GPIO.input(pin)
     
     if state == 0:
-        GPIO.output(pin,GPIO.HIGH)
-        return jsonify({'status':'LOW', 'pin_no':pin})
+        #GPIO.output(pin,GPIO.HIGH)
+        return jsonify({'status':'HIGH', 'pin_no':pin})
     
     else:
-        GPIO.output(pin,GPIO.LOW)
-        return jsonify({'status':'HIGH', 'pin_no':pin})
+        #GPIO.output(pin,GPIO.LOW)
+        return jsonify({'status':'LOW', 'pin_no':pin})
 
 if __name__ =='__main__':
     app.run(host='0.0.0.0', debug=True)
